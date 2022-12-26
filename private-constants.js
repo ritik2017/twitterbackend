bd5bc5c2e6746dbdd6651ca03c445c110ff3f728
Ritik Kumar <kumarritik@microsoft.com>
2022-12-26 15:49:24 +0530

    Tokens

---
+++ private-constants.js
@@ -0,0 +1,4 @@
+const MONGODBURI = `mongodb+srv://myappuser:ritikkumar@cluster0.mjfcg.mongodb.net/twitterjs?retryWrites=true&w=majority`;
+const SESSIONKEY = `hello167world89568au1123`;
+
+module.exports = { MONGODBURI, SESSIONKEY };
